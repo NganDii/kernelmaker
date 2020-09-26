3dea59ff319114b3fddf344f09fe8da5a2e7f85d
RemApple <32689703+NganDii@users.noreply.github.com>
2020-09-26 13:59:50 +0530

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,7 +1,8 @@
+aik=/data/local/AIK-mobile
 echo "KERNEL MAKER"
 rm -rf ~/anykernel
 rm -rf /sdcard/Kernels/anykernel
-mkdir /sdcard/Kernels
+test ! -d /sdcard/Kernels && mkdir -p /sdcard/Kernels
 mkdir /sdcard/Kernels/anykernel
 git clone https://github.com/NganDii/anykernel
 mv -f $HOME/anykernel/anykernel.zip /sdcard/Kernels/anykernel
@@ -16,12 +17,11 @@ echo "Press y to continue"
 read r
 if [ $r == "y" ]; then
 su -c "rm -rf /data/local/AIK-mobile/boot.img"
-su -c cp -f /sdcard/Kernels/boot.img /data/local/AIK-mobile/
+su -c mv -f /sdcard/Kernels/boot.img /data/local/AIK-mobile/
 su -c "(cd /data/local/AIK-mobile && ./unpackimg.sh)"
 su -c "(mv  -f /data/local/AIK-mobile/split_img/boot.img-zImage /sdcard/Kernels/anykernel/zImage)"
 su -c "(cd /data/local/AIK-mobile/ && ./cleanup.sh)"
 su -c rm -rf /data/local/AIK-mobile/boot.img
-su -c rm /sdcard/Kernels/boot.img
 cd /sdcard/Kernels/anykernel
 echo
 echo "Enter you desired Kernel Name without space"
