75dceb85ab7b2d1de9ef1a0be5c3f049710bfc02
RemApple <32689703+NganDii@users.noreply.github.com>
2020-09-26 14:23:26 +0530

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,5 +1,7 @@
+
 bin=/data/data/com.termux/files/usr/bin
 cd ~/kernelmaker
-su -c cp kernel.sh $bin/kmaker && su -c chmod +x $bin/kmaker
-rm ~/kernelmaker
+su -c "cp kernel.sh $bin/kmaker" && chmod +x $bin/kmaker
+rm -rf ~/kernelmaker
 echo "Successful! Type "kmaker" "
+
